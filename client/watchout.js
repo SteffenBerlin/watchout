061b60ba1f05b496f910ed174202270cdca06d37
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-22 19:05:16 -0800

    Implement update function to enable enemy movement

--- client/watchout.js
+++ client/watchout.js
@@ -1,24 +1,47 @@
+
+
 var enemyData = [[10, 200], [50, 50], [300, 45]];
 
 
 var svg = d3.select('.board').append('svg')
             .attr('width', '500')
             .attr('height', '500');
-// var enemies = d3.select('.board').selectAll('svg')
-//   .data(enemyData)
-//   .enter()
-//   .append('svg')
-//   .attr('class', 'enemy');
 
 
-// enemies.style()
 
-var enemies = svg.selectAll('circle')
-                 .data(enemyData)
-                 .enter()
-                 .append('circle');
 
-enemies.attr('fill', 'yellow')
-       .attr('cx', function(d){return d[1];})
-       .attr('cy', function(d) {return d[0];})
-       .attr('r', '40');
+var updateEnemies = function(enemyData) {
+
+  var enemies = svg.selectAll('circle')
+                 .data(enemyData, function(i) {return i;});
+
+  // enemies.attr('class', 'update');
+
+  enemies.enter().append('circle')
+               .attr('class', 'enter')
+               .attr('fill', 'yellow')
+               .attr('cx', function(d){return d[1];})
+               .attr('cy', function(d) {return d[0];})
+               .attr('r', '40');
+
+  enemies.exit().remove();
+};
+
+var newEnemyData = function(){
+  function randomHelper(){
+    return [Math.floor(Math.random() * 500), Math.floor(Math.random() * 500)];
+  }
+  for(var i = 0; i < enemyData.length; i++){
+    enemyData[i]=randomHelper();
+  }
+  updateEnemies(enemyData);
+};
+
+
+
+
+
+setInterval(function() {
+  newEnemyData();
+  }, 1000);
+
