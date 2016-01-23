8bcf0dbf3a18bd36a17cb35c3593dfa647a57bd8
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-23 12:35:00 -0800

    create CheckCollision

--- client/watchout.js
+++ client/watchout.js
@@ -11,38 +11,21 @@ var svg = d3.select('.board').append('svg')
 
 
 var enemyData = new Array(numberEnemies);
-var drag = d3.behavior.drag()  
-             .on('dragstart', function() { player.style('fill', '#94778B'); })
-             .on('drag', function() { player.attr('cx', function(){
-                                                          if(d3.event.x <= 0) {
-                                                            return 10;
-                                                          } else if(d3.event.x >= 500) {
-                                                            return 490;
-                                                          } else {
-                                                            return d3.event.x;
-                                                          }})
-                                            .attr('cy', function(){
-                                                          if(d3.event.y <= 0) {
-                                                            return 10;
-                                                          } else if(d3.event.y >= 500) {
-                                                            return 490;
-                                                          } else {
-                                                            return d3.event.y;
-                                                          }}); })
-             .on('dragend', function() { player.style('fill', '#54494B'); });
 
 var enemies;
+
 var player;
+
 var createEnemies = function(enemyData) {
   enemies = svg.selectAll('circle')
-                 .data(enemyData)
-                 .enter()
-                 .append('circle')
-                 .attr('class', 'enter')
-                 .attr('fill', '#FF8C42')
-                 .attr('cx', function(d){return d[1];})
-                 .attr('cy', function(d) {return d[0];})
-                 .attr('r', '10');
+               .data(enemyData)
+               .enter()
+               .append('circle')
+               .attr('class', 'enter')
+               .attr('fill', '#FF8C42')
+               .attr('cx', function(d){return d[1];})
+               .attr('cy', function(d) {return d[0];})
+               .attr('r', '10');
 };
 
 var initialize = function(noEn, enemData){
@@ -67,7 +50,6 @@ var moveEnemies = function(enemyData) {
          .duration(500)
          .attr('cx', function(d){return d[1];})
          .attr('cy', function(d) {return d[0];});
-         
 
   // enemies.exit().remove();
 };
@@ -82,6 +64,42 @@ var newEnemyData = function(){
   moveEnemies(enemyData);
 };
 
+var drag = d3.behavior.drag()  
+             .on('dragstart', function() { player.style('fill', '#94778B'); })
+             .on('drag', function() { player.attr('cx', function(){
+                                                          if(d3.event.x <= 0) {
+                                                            return 10;
+                                                          } else if(d3.event.x >= 500) {
+                                                            return 490;
+                                                          } else {
+                                                            return d3.event.x;
+                                                          }})
+                                            .attr('cy', function(){
+                                                          if(d3.event.y <= 0) {
+                                                            return 10;
+                                                          } else if(d3.event.y >= 500) {
+                                                            return 490;
+                                                          } else {
+                                                            return d3.event.y;
+                                                          }}); })
+             .on('dragend', function() { player.style('fill', '#54494B'); });
+
+var distanceHelper = function(x1, y1, x2, y2){
+    return Math.sqrt(Math.pow(Math.abs(x1-x2), 2) + Math.pow(Math.abs(y1-y2), 2));
+  };
+
+var checkCollisions = function(){
+  enemies.each(function(el){
+    // console.log(el[0]);
+    // console.log(el[1]);
+    // console.log(distanceHelper(el[0], el[1], parseInt(player.attr('cx')), parseInt(player.attr('cy'))));
+    if(distanceHelper(el[0], el[1], parseInt(player.attr('cx')), parseInt(player.attr('cy'))) < 20){
+      player.style('fill', '#'+(Math.random()*0xFFFFFF<<0).toString(16));
+      console.log('COLLISION');
+    }
+  });
+};
+
 
 
 initialize(numberEnemies, enemyData);
@@ -90,3 +108,6 @@ setInterval(function() {
   newEnemyData();
   }, 1000);
 
+setInterval(function() {
+  checkCollisions();
+  }, 50);
